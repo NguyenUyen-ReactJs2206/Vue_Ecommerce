c1bb6b02241d4b357339c09434264241ce367996
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-23 00:59:48 +0700

    layout page footer

--- vue-project/src/components/Footer/Footer.vue
+++ vue-project/src/components/Footer/Footer.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h3>Footer</h3>
-  </div>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__content">
+        <div class="footer__content--first-column">
+          <div class="first-content">© 2023 Shopee. Tất cả các quyền được bảo lưu.</div>
+          <div class="second-content">
+            Quốc gia & Khu vực: Singapore Indonesia Đài Loan Thái Lan Malaysia Việt Nam Philippines Brazil México
+            Colombia Chile
+          </div>
+        </div>
+        <div class="footer__content--second-column">
+          <div>Công ty TNHH Shopee</div>
+          <div>
+            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Khánh, Quận Ba Đình, Thành
+            phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 19001221 - Email: cskh@hotro.shopee.vn
+          </div>
+          <div>Chịu Trách Nhiệm Quản Lý Nội Dung: Nguyễn Đức Trí - Điện thoại liên hệ: 024 73081221 (ext 4678)</div>
+          <div>Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp lần đầu ngày 10/02/2015</div>
+          <div>© 2015 - Bản quyền thuộc về Công ty TNHH Shopee</div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
