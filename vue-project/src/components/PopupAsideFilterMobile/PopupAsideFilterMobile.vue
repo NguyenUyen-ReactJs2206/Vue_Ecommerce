08c812f9a851e6b4661fc403d14fa8b625676d92
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-26 23:53:31 +0700

    demo show Popup

---
+++ vue-project/src/components/PopupAsideFilterMobile/PopupAsideFilterMobile.vue
@@ -0,0 +1,101 @@
+<template>
+  <teleport to="body">
+    <div class="overlay" @click.self="$emit('closePopup')">
+      <div class="asideFilter">
+        <div class="asideFilter__category">
+          <router-link :to="`${path.home}`" class="asideFilter__link asideFilter__link--flex">
+            <svg viewBox="0 0 12 10" class="asideFilter__icon">
+              <g fillRule="evenodd" stroke="none" strokeWidth="1">
+                <g transform="translate(-373 -208)">
+                  <g transform="translate(155 191)">
+                    <g transform="translate(218 17)">
+                      <path d="m0 2h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z"></path>
+                      <path d="m0 6h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z"></path>
+                      <path d="m0 10h2v-2h-2zm4 0h7.1519633v-2h-7.1519633z"></path>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            Tất cả danh mục
+          </router-link>
+          <div className="asideFilter__line" />
+          <ul class="asideFilter__category-list">
+            <li class="asideFilter__category-item">Đồng hồ</li>
+            <li class="asideFilter__category-item">Áo thun</li>
+            <li class="asideFilter__category-item">Điện thoại</li>
+          </ul>
+        </div>
+
+        <div class="asideFilter__search-filter">
+          <router-link
+            :to="`${path.home}`"
+            class="asideFilter__link asideFilter__link--flex asideFilter__link--uppercase"
+          >
+            <svg
+              class="asideFilter__icon asideFilter__icon--current-color"
+              enable-background="new 0 0 15 15"
+              viewBox="0 0 15 15"
+              x="0"
+              y="0"
+            >
+              <g>
+                <polyline
+                  fill="none"
+                  points="5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="10"
+                ></polyline>
+              </g>
+            </svg>
+            Bộ lọc tìm kiếm
+          </router-link>
+          <div className="asideFilter__line" />
+          <div class="asideFilter__price-range">Khoản giá</div>
+          <form action="">
+            <div class="asideFilter__price-form-input">
+              <input type="number" placeholder="₫ TỪ" class="asideFilter__price-input" />
+              <div class="asideFilter__price-divider">-</div>
+              <input type="number" placeholder="₫ ĐẾN" class="asideFilter__price-input" />
+            </div>
+
+            <button class="asideFilter__apply-button">Áp dụng</button>
+          </form>
+        </div>
+
+        <div className="asideFilter__line" />
+
+        <div class="asideFilter__rating-start">
+          <div class="asideFilter__rating-title">Đánh giá</div>
+          <RatingStart />
+        </div>
+
+        <div className="asideFilter__line" />
+
+        <button class="asideFilter__reset-button">Xóa tất cả</button>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+import path from '../../constants/path';
+import RatingStart from '../../pages/ProductList/components/RatingStart/RatingStart.vue';
+
+defineEmits(['closePopup']);
+</script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(158, 158, 158, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- vue-project/src/pages/ProductList/ProductList.vue
+++ vue-project/src/pages/ProductList/ProductList.vue
@@ -1,22 +1,48 @@
-<script setup>
-import AsideFilter from './components/AsideFilter/AsideFilter.vue';
-import SortProductList from './components/SortProductList/SortProductList.vue';
-</script>
-
 <template>
   <div class="product-list">
     <div class="container">
       <div class="product-list__main">
         <div class="product-list__main-left">
-          <AsideFilter />
+          <AsideFilter v-if="isVisible" />
+          <button v-if="!isVisible" @click="onShowPopup">SHOW POPUP</button>
         </div>
         <div class="product-list__main-right">
           <SortProductList />
         </div>
       </div>
     </div>
+
+    <PopupAsideFilterMobile v-if="showPopupAsideFilterMobile" @closePopup="onClosePopup" />
   </div>
 </template>
+
+<script setup>
+import PopupAsideFilterMobile from 'src/components/PopupAsideFilterMobile/PopupAsideFilterMobile.vue';
+import AsideFilter from './components/AsideFilter/AsideFilter.vue';
+import SortProductList from './components/SortProductList/SortProductList.vue';
+import { Teleport, ref } from 'vue';
+
+const isVisible = ref(false);
+const showPopupAsideFilterMobile = ref(false);
+
+const onShowPopup = () => {
+  showPopupAsideFilterMobile.value = !showPopupAsideFilterMobile.value;
+};
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    isVisible.value = false;
+    showPopupAsideFilterMobile.value = false;
+  } else {
+    isVisible.value = true;
+    showPopupAsideFilterMobile.value = false;
+  }
+});
+
+const onClosePopup = () => {
+  showPopupAsideFilterMobile.value = false;
+};
+</script>
 
 <style scoped>
 h2 {
