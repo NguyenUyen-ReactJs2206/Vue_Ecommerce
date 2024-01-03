3e0848c9c6bf208ee5657a0b61c91fb7ac7e9a9f
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2024-01-04 00:58:18 +0700

    setup pinia

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -4,7 +4,11 @@ import 'src/assets/css/global.scss';
 import App from './App.vue';
 import router from './router';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
+const pinia = createPinia();
 const app = createApp(App);
+
 app.use(router);
+app.use(pinia);
 app.mount('#app');
